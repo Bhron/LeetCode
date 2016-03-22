446b8e6276d4241c4c753efbd8c8e100e10a3fe4
Jianhao Guo <gjhbyron@gmail.com>
2016-03-21 18:49:32 -0700

    Add C++ solution for problem 78.Subsets.

---
+++ Algorithms/C++/Backtracking/78_Subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> all_subsets;
+
+        if (nums.empty()) {
+            return all_subsets;
+        }
+
+        sort(nums.begin(), nums.end());
+        vector<int> init_set;
+
+        helper(all_subsets, init_set, nums, 0);
+
+        return all_subsets;
+    }
+private:
+    void helper(vector<vector<int>>& all_subsets, vector<int>& cur_set, vector<int>& nums, int pos) {
+        all_subsets.push_back(cur_set);
+
+        for (int i = pos; i < nums.size(); i++) {
+            cur_set.push_back(nums[i]);
+            helper(all_subsets, cur_set, nums, i + 1);
+            cur_set.pop_back();
+        }
+    }
+};
