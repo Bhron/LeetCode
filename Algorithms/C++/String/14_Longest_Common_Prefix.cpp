2965c78234ee8a334c05d2ae72ccdf82e3f1fa95
Jianhao Guo <gjhbyron@gmail.com>
2016-03-23 20:10:25 -0700

    Add C++ solution for problem 14.Longest Common Prefix.

---
+++ Algorithms/C++/String/14_Longest_Common_Prefix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix;
+
+        if (strs.empty()) {
+            return prefix;
+        }
+
+        int min_len = INT_MAX;
+        for (int i = 0; i < strs.size(); i++) {
+            if (strs[i].length() < min_len) {
+                min_len = strs[i].length();
+            }
+        }
+
+
+
+        for (int i = 0; i < min_len; i++) {
+            for (int j = 1; j < strs.size(); j++) {
+                if (strs[j][i] != strs[0][i]) {
+                    return prefix;
+                }
+            }
+
+            prefix += strs[0][i];
+        }
+
+        return prefix;
+    }
+};
