f632f46f47a58ba540c446f580d985cf7302d622
Jianhao Guo <gjhbyron@gmail.com>
2016-03-28 20:41:33 -0700

    Add Swift solution for problem 78.Subsets.

---
+++ Algorithms/Swift/Backtracking/78_Subsets.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func subsets(nums: [Int]) -> [[Int]] {
+        var allSubsets = [[Int]]()
+
+        if nums.isEmpty {
+            return allSubsets
+        }
+
+        var sortedNums = nums
+        sortedNums.sortInPlace()
+        var currentSubset = [Int]()
+
+        helper(&allSubsets, &currentSubset, sortedNums, 0)
+
+        return allSubsets
+    }
+
+    private func helper(inout allSubsets: [[Int]], inout _ currentSubset: [Int], _ nums: [Int], _ position: Int) {
+        allSubsets.append(currentSubset)
+
+        for var i = position; i < nums.count; i++ {
+            currentSubset.append(nums[i])
+            helper(&allSubsets, &currentSubset, nums, i + 1)
+            currentSubset.removeLast()
+        }
+    }
+}
