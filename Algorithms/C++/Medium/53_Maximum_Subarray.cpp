55540e3aafb42817f54282733a766b48d2d9370e
Jianhao Guo <gjhbyron@gmail.com>
2016-04-03 18:07:00 -0700

    Modify some variables' names to make it more understandable.

--- Algorithms/C++/Medium/53_Maximum_Subarray.cpp
+++ Algorithms/C++/Medium/53_Maximum_Subarray.cpp
@@ -5,11 +5,11 @@ public:
             return 0;
         }
 
-        int largest_sum = INT_MIN, min_sum = 0, sum = 0;
+        int largest_sum = INT_MIN, min_prefix_sum = 0, prefix_sum = 0;
         for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            largest_sum = max(largest_sum, sum - min_sum);
-            min_sum = min(min_sum, sum);
+            prefix_sum += nums[i];
+            largest_sum = max(largest_sum, prefix_sum - min_prefix_sum);
+            min_prefix_sum = min(min_prefix_sum, prefix_sum);
         }
 
         return largest_sum;
