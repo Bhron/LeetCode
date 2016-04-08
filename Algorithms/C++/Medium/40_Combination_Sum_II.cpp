27770d91e712dbab3a39ea0732726b1d07976bd1
Jianhao Guo <gjhbyron@gmail.com>
2016-04-07 22:01:52 -0700

    Move solution for problem 40.Combination Sum II.

---
+++ Algorithms/C++/Medium/40_Combination_Sum_II.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> combinations;
+
+        if (candidates.empty()) {
+            return combinations;
+        }
+
+        sort(candidates.begin(), candidates.end());
+        vector<int> cur;
+
+        helper(combinations, cur, 0, candidates, target);
+
+        return combinations;
+    }
+private:
+    void helper(vector<vector<int>>& combinations, vector<int>& cur, int pos, vector<int>& candidates, int target) {
+        if (target == 0) {
+            combinations.push_back(cur);
+            return;
+        }
+
+        int prev = -1;
+        for (int i = pos; i < candidates.size(); i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+
+            if (prev != -1 && prev == candidates[i]) {
+                continue;
+            }
+
+            cur.push_back(candidates[i]);
+            helper(combinations, cur, i + 1, candidates, target - candidates[i]);
+            cur.pop_back();
+
+            prev = candidates[i];
+        }
+    }
+};
