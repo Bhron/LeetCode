173d157ed66cb5edfd04ed5aa62e728b0a0ef7cf
Jianhao Guo <gjhbyron@gmail.com>
2016-04-18 01:04:43 -0700

    Move solution for problem 2.Add Two Numbers and add a tag.

---
+++ Algorithms/C++/Medium/2_Add_Two_Numbers.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+
+        ListNode* node = &dummy;
+        int carry = 0;
+        while (l1 != NULL || l2 != NULL || carry != 0) {
+            int sum = 0;
+
+            sum += carry;
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            node->next = new ListNode(sum % 10);
+            node = node->next;
+
+            carry = sum / 10;
+        }
+
+        return dummy.next;
+    }
+};
