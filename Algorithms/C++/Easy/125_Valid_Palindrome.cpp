1bc67aae75d43a75e6d3c87273c382e1ba8014c4
Jianhao Guo <gjhbyron@gmail.com>
2016-04-22 18:25:04 -0700

    Move solution for problem 125.Valid Palindrom and add a tag.

---
+++ Algorithms/C++/Easy/125_Valid_Palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.empty()) {
+            return true;
+        }
+
+        int start = 0, end = s.length() - 1;
+        while (start < end) {
+            if (!isalnum(s[start])) {
+                start++;
+            } else if (!isalnum(s[end])) {
+                end--;
+            } else if (tolower(s[start]) != tolower(s[end])) {
+                return false;
+            } else {
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+};
