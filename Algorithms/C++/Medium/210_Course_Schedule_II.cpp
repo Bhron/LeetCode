bcf41b5075b4dcae3ade8982fbe5dec48f5a182c
Jianhao Guo <gjhbyron@gmail.com>
2016-05-11 23:42:53 -0700

    Add another solution for problem 210.Course Schedule II.

--- Algorithms/C++/Medium/210_Course_Schedule_II.cpp
+++ Algorithms/C++/Medium/210_Course_Schedule_II.cpp
@@ -1,3 +1,4 @@
+// BFS
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
@@ -63,5 +64,77 @@ private:
         }
 
         return nodes;
+    }
+};
+
+// DFS
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<int> courses(numCourses, -1);
+        
+        if (numCourses <= 0) {
+            return courses;
+        }
+        
+        vector<DirectedGraphNode*> nodes = constructGraph(numCourses, prerequisites);
+        
+        vector<int> visited(numCourses, 0);
+        current_pos = numCourses - 1;
+        for (int i = 0; i < nodes.size(); i++) {
+            DirectedGraphNode* node = nodes[i];
+            if (visited[node->label] == 0) {
+                if (DFS(nodes, visited, node, courses) == false) {
+                    return vector<int>();
+                }
+            }
+        }
+        
+        return courses;
+    }
+
+private:
+    class DirectedGraphNode {
+    public:
+        int label;
+        unordered_set<int> neighbors;
+        
+        DirectedGraphNode(int label) : label(label) { }
+    };
+    
+    vector<DirectedGraphNode*> constructGraph(int n, const vector<pair<int, int>>& prerequisites) {
+        vector<DirectedGraphNode*> nodes;
+        for (int i = 0; i < n; i++) {
+            nodes.push_back(new DirectedGraphNode(i));
+        }
+        
+        for (int i = 0; i < prerequisites.size(); i++) {
+            int course = prerequisites[i].first;
+            int prerequisite = prerequisites[i].second;
+            nodes[prerequisite]->neighbors.insert(course);
+        }
+        
+        return nodes;
+    }
+    
+    int current_pos;
+    
+    bool DFS(vector<DirectedGraphNode*>& nodes, vector<int>& visited, DirectedGraphNode* node, vector<int>& courses) {
+        visited[node->label] = -1;
+        
+        for (const auto& neighbor: node->neighbors) {
+            if (visited[neighbor] == -1) {
+                return false;
+            } else if (visited[neighbor] == 0) {
+                if (DFS(nodes, visited, nodes[neighbor], courses) == false) {
+                    return false;
+                }
+            }
+        }
+        
+        visited[node->label] = 1;
+        courses[current_pos--] = node->label;
+        
+        return true;
     }
 };
