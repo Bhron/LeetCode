f405681bd2716ae9bed03a9367ec5b95d405a530
Jianhao Guo <gjhbyron@gmail.com>
2016-06-04 09:29:12 -0400

    Change the solution for problem 168.Excel Sheet Column Title.

--- Algorithms/C++/Easy/168_Excel_Sheet_Column_Title.cpp
+++ Algorithms/C++/Easy/168_Excel_Sheet_Column_Title.cpp
@@ -8,17 +8,9 @@ public:
         }
 
         while (n > 0) {
-            int curCol = n % 26;
-            if (curCol == 0) {
-                curCol = 26;
-            }
-
-            title += 'A' + curCol - 1;
-
+            n--;
+            title += 'A' + n % 26;
             n /= 26;
-            if (curCol == 26) {
-                n--;
-            }
         }
 
         reverse(title.begin(), title.end());
