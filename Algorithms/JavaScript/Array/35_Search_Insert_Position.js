c29fd9c18df6242a608a096f0b6478f031607849
Jianhao Guo <gjhbyron@gmail.com>
2016-08-29 15:47:28 -0400

    Add JavaScript solution for problem 35.Search Insert  Position.

---
+++ Algorithms/JavaScript/Array/35_Search_Insert_Position.js
@@ -0,0 +1,1 @@
+../Medium/35_Search_Insert_Position.js
---
+++ Algorithms/JavaScript/Medium/35_Search_Insert_Position.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    if (nums.length === 0) {
+        return 0
+    }
+    
+    var start = 0, end = nums.length - 1
+    
+    while (start + 1 < end) {
+        var mid = start + Math.floor((end - start) / 2)
+        if (nums[mid] >= target) {
+            end = mid
+        } else {
+            start = mid
+        }
+    }
+    
+    if (nums[start] >= target) {
+        return start
+    } else if (nums[end] >= target) {
+        return end
+    }
+    return nums.length
+};
