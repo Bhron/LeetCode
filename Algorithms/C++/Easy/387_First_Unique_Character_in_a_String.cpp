bf68d01ddeac287a1c1c5298b8f7850d3982b6c2
Jianhao Guo <gjhbyron@gmail.com>
2016-09-30 20:18:09 -0400

    Add C++ solution for problem 387.First Unique Character in a String.

---
+++ Algorithms/C++/Easy/387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        if (s.empty()) {
+            return -1;
+        }
+
+        unordered_map<char, int> hash;
+        for (int i = 0; i < s.length(); i++) {
+            hash[s[i]]++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (hash[s[i]] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
